bc7a76fa7a24bc8f4203c26b8cd5b55ad04f64c8
Admin <lxldevtest@163.com>
2021-08-09 16:30:54 +0800

    first commit!

--- git.js
+++ git.js
@@ -5,20 +5,26 @@ simpleGit = git(GIT_REPOSITORY_ROOT );
 
 async  function gitfun (){
     //以下的所有命令都是基于这个repository的路径进行操作了
-    await simpleGit.status();
+    let status
+    status = await simpleGit.status();
+    console.log(status)
     //等价于 git status
     // await simpleGit.pull('origin', 'master');  
     // 等价于 git pull origin marster (第一个参数是你的remote端名称， 第二个参数是你的分支名称)
     // await simpleGit.checkout('master');
     // 等价于 git checkout master (master 指的是你的分支名称)
-    await simpleGit.add('*');
+    status = await simpleGit.add('*');
+    console.log(status)
     // 等价于  git add ./*
-    await simpleGit.commit('first commit!');
+    status = await simpleGit.commit('first commit!');
+    console.log(status)
     // 等价于 git commit -m 'first commit!'
-    await simpleGit.push('origin', 'master');
+    status = await simpleGit.push('origin', 'master');
+    console.log(status)
     // 等价于 git push origin master (origin指的是你的remote端名称， master指的是你的分支名称)
     // await simpleGit.mergeFromTo('from', 'to');
     // 将from分支上的代码合并到to分支上去
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
     console.log('success')
     process.exit(0);//退出子进程
 }
